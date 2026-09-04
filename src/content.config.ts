f960da688e0e862eda0b2748a14bb2e412343607
ilenceye <ye66169@gmail.com>
2026-09-04 16:25:11 +0800

    feat: 删除"看过"板块

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,8 +1,6 @@
 import { defineCollection } from "astro:content";
 import { glob } from "astro/loaders";
 
-import { WatchedItemSchema } from "@/schemas";
-
 const links = defineCollection({
   loader: glob({ pattern: "**/*.md", base: "./content/links" }),
 });
@@ -15,13 +13,8 @@ const posts = defineCollection({
   loader: glob({ pattern: "**/*.md", base: "./content/posts" }),
 });
 
-const watched = defineCollection({
-  loader: glob({ pattern: "**/*.md", base: "./content/watched" }),
-  schema: WatchedItemSchema,
-});
-
 const words = defineCollection({
   loader: glob({ pattern: "**/*.md", base: "./content/words" }),
 });
 
-export const collections = { links, notes, watched, words, posts };
+export const collections = { links, notes, words, posts };
